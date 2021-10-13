129fae63b734c00a1e4b956fdf3a578f6e018364
GitHub Action <action@github.com>
2021-10-13 18:08:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/gervant08/mvimodulegenerator/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/gervant08/mvimodulegenerator/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gervant08.mvimodulegenerator.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.gervant08.mvimodulegenerator.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/gervant08/mvimodulegenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.gervant08.mvimodulegenerator.services
+
+import com.github.gervant08.mvimodulegenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
